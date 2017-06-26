b0fd03b7ab9ab2742a672b6da7708f1b89c66c9f
wang <wang@wang.local>
2017-06-26 16:36:58 +0800

    make some change to hello.go

--- hello.go
+++ hello.go
@@ -1,5 +1,38 @@
 package main
+
 import "fmt"
-func main(){
+
+func main() {
 	fmt.Println("hello world go")
+	chan1 := make(chan bool, 2)
+	chan2 := make(chan int, 1)
+	chan1 <- false
+	select {
+	case b := <-chan1:
+		// b <- chan1
+		fmt.Println(b, "striopss")
+	case chan2 <- 3:
+		fmt.Println(<-chan2, "int")
+	default:
+		fmt.Println("exit select")
+	}
+
+	s := []int{1, 2, 3, 4, 5, 6}
+	c := make(chan int, 2)
+	go sunArr(s[:len(s)/2], c)
+	// z := <-c
+	go sunArr(s[len(s)/2:], c)
+	// y := <-c
+	// x, y := <-c, <-c
+	// fmt.Println(x, y)
+}
+
+func sunArr(arr []int, c chan int) {
+	// defer close(c)
+	sum := 0
+	for _, element := range arr {
+		sum += element
+		fmt.Println(sum)
+	}
+	c <- sum
 }
